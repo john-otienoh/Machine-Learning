1f54b5d17d4c58de4d25e93b889574215c544b2e
john-otienoh <johnteclaire@gmail.com>
2025-04-15 18:58:32 +0300

    Working with Vectors, Matrices, and Arrays in NumPy

--- ml-with-python-cookbook/Handling-Numerical-Data/polynomial.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-from sklearn.preprocessing import PolynomialFeatures
-
-features = np.array([[2, 3],
-[2, 3],
-[2, 3]])
-polynomial_interaction = PolynomialFeatures(degree=2, include_bias=False)
-polynomial_interaction.fit_transform(features)
